9ed0a8aad966e74741b5b65f21d236cb4e6d6228
Treesa17 <sajut@sheridancollege.ca>
2023-01-23 11:32:50 -0500

    ICE-1

---
+++ src/exercise1/Exercise1.java
@@ -0,0 +1,13 @@
+
+package exercise1;
+
+public class Exercise1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
