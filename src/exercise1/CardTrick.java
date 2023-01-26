029fd925a5a184be9b355969d4e39c3b55f38968
Treesa Saju <122334012+Treesa17@users.noreply.github.com>
2023-01-25 22:14:38 -0500

    Update CardTrick.java

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -65,19 +65,20 @@ public class CardTrick {
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
 
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
+        System.out.println("My name is Treesa saju, but you can call me Tree");
         System.out.println();
 
         System.out.println("My career ambitions:");
-        System.out.println("-- Be more active on LinkedIn");
-        System.out.println("-- Have a semester with no violations of academic integrity!");
+        System.out.println("-- Be an professional software enginner ");
+        System.out.println("-- Have a good family");
+         System.out.println("--Owning a bussiness");
         System.out.println();
 
         System.out.println("My hobbies:");
-        System.out.println("-- Investing");
+        System.out.println("-- Gardening");
         System.out.println("-- Cooking");
-        System.out.println("-- Reading/Watching TV");
-        System.out.println("-- Riding my motorcycle");
+        System.out.println("-- Music/Watching TV");
+        System.out.println("--Badminton ");
 
     }
 
